47430256d6ca4c0e37b202ab06ac8e3b7d593696
Shokunin San <me@flatmap.io>
2015-02-08 00:12:41 +0100

    Refactored plotting code.

--- src/main/scala/LinearRegression.scala
+++ src/main/scala/LinearRegression.scala
@@ -9,14 +9,26 @@ object LinearRegression {
   type Labels = DenseVector[Double]
   type Prediction = DenseVector[Double]
 
-  def plotData(X: DenseVector[Double], y: DenseVector[Double]): Figure = {
-    val f = Figure()
-    f.subplot(0) +=  scatter(X, y, {(_:Int) => 0.1})
-    f.subplot(0).ylabel = "Profit in $10,000s"
-    f.subplot(0).xlabel = "Population of City in 10,000s"
-    f.subplot(0).title = "Ex1 Data"
-    f.refresh()
-    f
+  def plotData(figure: Figure, X: DenseVector[Double], y: DenseVector[Double]): Unit = {
+    figure.subplot(0) +=  scatter(X, y, {(_:Int) => 0.1})
+    figure.subplot(0).ylabel = "Profit in $10,000s"
+    figure.subplot(0).xlabel = "Population of City in 10,000s"
+    figure.subplot(0).title = "Linear Regression Model"
+    figure.refresh()
+  }
+
+  def plotHypothesis(figure: Figure, X: Features, theta: Theta): Unit = {
+    figure.subplot(0) += plot(X(::,1), X * theta)
+    figure.refresh()
+  }
+
+  def plotError(errors: J): Unit = {
+    val figure = Figure("Gradient Descent")
+    figure.subplot(0) += plot(Array.tabulate(errors.length){_.toDouble}, errors)
+    figure.subplot(0).ylabel = "J(θ)"
+    figure.subplot(0).xlabel = "Iterations"
+    figure.subplot(0).title = "Gradient Descent"
+    figure.refresh()
   }
 
   def h(theta: Theta): Features => Prediction = (X: Features) => X * theta
@@ -40,18 +52,18 @@ object LinearRegression {
   }
 
   def main(args: Array[String]): Unit = {
+    val f = Figure("Linear Regression Model")
     val data = csvread(new java.io.File("resources/ex1data1.txt"))
     val x = data(::,0)
     val y = data(::,1)
     val m = y.length
     val X = DenseMatrix(DenseVector.ones[Double](m).toArray, data(::,0).toArray).t
     val theta = DenseVector.zeros[Double](2)
-    val f = plotData(x, y) // scatter plot training data
+    plotData(f, x, y) // scatter plot training data
     gradientDescent(X, y, theta, 0.01, 1500) match {
-      case tpl =>
-        println(s"theta: ${tpl._1}")
-        f.subplot(0) += plot(X(::,1), X * tpl._1)
-        f.refresh // plot linear hypothesis function
+      case (theta: Theta, history: J) =>
+        plotHypothesis(f, X, theta)
+        plotError(history)
     }
   }
 
