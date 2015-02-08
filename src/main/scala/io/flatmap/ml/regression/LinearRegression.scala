e3baa76aa68e345c25de80c13ab748f9ed3f984b
Shokunin San <me@flatmap.io>
2015-02-08 02:22:29 +0100

    Minor refactoring.

--- src/main/scala/io/flatmap/ml/regression/LinearRegression.scala
+++ src/main/scala/io/flatmap/ml/regression/LinearRegression.scala
@@ -4,24 +4,23 @@ import breeze.linalg._
 
 object LinearRegression extends RegressionModel with Optimization {
 
-  private final val data = csvread(new java.io.File("resources/ex1data1.txt"))
+  lazy val data = csvread(new java.io.File("resources/ex1data1.txt"))
+  lazy val x = data(::,0)
+  lazy val y = data(::,1)
+  lazy val m = y.length
+  lazy val X = DenseMatrix(DenseVector.ones[Double](m).toArray, data(::,0).toArray).t
+  lazy val theta = DenseVector.zeros[Double](2)
 
   override def h(theta: Theta): Features => Prediction = (X: Features) => X * theta
 
   override def computeCost(X: Features, y: Labels, theta: Theta): Double = {
-    val m = y.length  // # of samples
     val mse = sum((h(theta)(X) - y) :^ 2.0)
     (1.0/(2.0*m)) * mse
   }
 
   def main(args: Array[String]): Unit = {
-    val x = data(::,0)
-    val y = data(::,1)
-    val m = y.length
-    val X = DenseMatrix(DenseVector.ones[Double](m).toArray, data(::,0).toArray).t
-    val theta = DenseVector.zeros[Double](2)
     Plot.data("Profit/Population", x, y) // scatter plot training data
-    gradientDescent(X, y, theta, 0.01, 1500) match {
+    gradientDescent(X, y, theta, alpha = 0.01, iterations = 1500) match {
       case (theta: Theta, history: J) =>
         Plot.hypothesis("Gradient Descent", X, theta)
         Plot.error(history)
